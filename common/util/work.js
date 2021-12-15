6fb0fcab4de8080d3335f37922084655b87bd4fe
furenjie <frj9641@163.com>
2021-12-15 15:13:49 +0800

    首页样式修改

--- common/util/work.js
+++ common/util/work.js
@@ -2,116 +2,116 @@
  * 常用服务
  * useful server
  */
-const  icon_prefix="/static/home/128/"
+const icon_prefix = "/static/home/128/"
 
 export const us = {
-  data:[
-   //  {
-   //    title:"日报",
-   //    icon:icon_prefix+"richang.png",
-   //    description:"记录每天的工作经验和心得",
-   //    useCount:1000,
-	  // page:'helloWorld'
-   //  },{
-   //    title:"周报",
-   //    icon:icon_prefix+"zhoubao.png",
-   //    description:"总结每周的工作情况和下周计划",
-   //    useCount:10000,
-	  // page:'helloWorld'
-   //  },{
-   //    title:"考勤",
-   //    icon:icon_prefix+"kaoqin.png",
-   //    description:"工作考勤",
-   //    useCount:10000,
-	  // page:'helloWorld'
-   //  },{
-   //    title:"日程",
-   //    icon:icon_prefix+"richeng.png",
-   //    description:"建立和查看个人工作安排",
-   //    useCount:10000,
-	  // page:'helloWorld'
-   //  },{
-   //    title:"请假申请",
-   //    icon:icon_prefix+"qingjia1.png",
-   //    description:"请假申请",
-   //    useCount:10000,
-	  // page:'helloWorld'
-   //  },{
-	  // title:"出差申请",
-	  // icon:icon_prefix+"chuchai.png",
-	  // description:"出差申请",
-	  // useCount:10000,
-	  // page:'helloWorld'
-   //  },{
-	  // title:"公文发文",
-	  // icon:icon_prefix+"gongwen.png",
-	  // description:"公文发文",
-	  // useCount:10000,
-	  // page:'helloWorld'
-   //  },{
-	  // title:"通知公告",
-	  // icon:icon_prefix+"tongzhi.png",
-	  // description:"查看企业对员工下发的通知公告",
-	  // useCount:10000,
-	  // page:'annotationList'
-   //  },{
-	  // title:"内部邮件",
-	  // icon:icon_prefix+"youjian.png",
-	  // description:"查看内部消息",
-	  // useCount:10000,
-	  // dot:false,
-	  // page:'helloWorld'
-   //  },{
-	  // title:"通讯录",
-	  // icon:icon_prefix+"tongxun.png",
-	  // description:"查看部门，组员",
-	  // useCount:10000,
-	  // page:'levelAddressBook'
-   //  },
-   {
-	  title:"采购业务",
-	  icon:icon_prefix+"hetong.png",
-	  description:"采购业务",
-	  useCount:10000,
-	  page:'purchase'
-    },{
-	  title:"入库业务",
-	  icon:icon_prefix+"hetong.png",
-	  description:"入库业务",
-	  useCount:10000,
-	  page:'purchase'
-    },{
-	  title:"出库业务",
-	  icon:icon_prefix+"hetong.png",
-	  description:"出库业务",
-	  useCount:10000,
-	  page:'purchase'
-    },{
-	  title:"产出品业务",
-	  icon:icon_prefix+"hetong.png",
-	  description:"产出品业务",
-	  useCount:10000,
-	  page:'purchase'
-    },{
-	  title:"入库质量数据采集",
-	  icon:icon_prefix+"hetong.png",
-	  description:"入库质量数据采集",
-	  useCount:10000,
-	  page:'purchase'
-    },{
-	  title:"产出品质量数据采集",
-	  icon:icon_prefix+"hetong.png",
-	  description:"产出品质量数据采集",
-	  useCount:10000,
-	  page:'purchase'
-    },{
-	  title:"质量数据采集",
-	  icon:icon_prefix+"hetong.png",
-	  description:"质量数据采集",
-	  useCount:10000,
-	  page:'dataCollect'
-    }
-  ]
+	data: [
+		//  {
+		//    title:"日报",
+		//    icon:icon_prefix+"richang.png",
+		//    description:"记录每天的工作经验和心得",
+		//    useCount:1000,
+		// page:'helloWorld'
+		//  },{
+		//    title:"周报",
+		//    icon:icon_prefix+"zhoubao.png",
+		//    description:"总结每周的工作情况和下周计划",
+		//    useCount:10000,
+		// page:'helloWorld'
+		//  },{
+		//    title:"考勤",
+		//    icon:icon_prefix+"kaoqin.png",
+		//    description:"工作考勤",
+		//    useCount:10000,
+		// page:'helloWorld'
+		//  },{
+		//    title:"日程",
+		//    icon:icon_prefix+"richeng.png",
+		//    description:"建立和查看个人工作安排",
+		//    useCount:10000,
+		// page:'helloWorld'
+		//  },{
+		//    title:"请假申请",
+		//    icon:icon_prefix+"qingjia1.png",
+		//    description:"请假申请",
+		//    useCount:10000,
+		// page:'helloWorld'
+		//  },{
+		// title:"出差申请",
+		// icon:icon_prefix+"chuchai.png",
+		// description:"出差申请",
+		// useCount:10000,
+		// page:'helloWorld'
+		//  },{
+		// title:"公文发文",
+		// icon:icon_prefix+"gongwen.png",
+		// description:"公文发文",
+		// useCount:10000,
+		// page:'helloWorld'
+		//  },{
+		// title:"通知公告",
+		// icon:icon_prefix+"tongzhi.png",
+		// description:"查看企业对员工下发的通知公告",
+		// useCount:10000,
+		// page:'annotationList'
+		//  },{
+		// title:"内部邮件",
+		// icon:icon_prefix+"youjian.png",
+		// description:"查看内部消息",
+		// useCount:10000,
+		// dot:false,
+		// page:'helloWorld'
+		//  },{
+		// title:"通讯录",
+		// icon:icon_prefix+"tongxun.png",
+		// description:"查看部门，组员",
+		// useCount:10000,
+		// page:'levelAddressBook'
+		//  },
+		{
+			title: "采购业务",
+			icon: icon_prefix + "hetong.png",
+			description: "采购业务",
+			useCount: 10000,
+			page: 'purchase'
+		}, {
+			title: "入库业务",
+			icon: icon_prefix + "hetong.png",
+			description: "入库业务",
+			useCount: 10000,
+			page: 'rk'
+		}, {
+			title: "出库业务",
+			icon: icon_prefix + "hetong.png",
+			description: "出库业务",
+			useCount: 10000,
+			page: 'ck'
+		}, {
+			title: "产出品业务",
+			icon: icon_prefix + "hetong.png",
+			description: "产出品业务",
+			useCount: 10000,
+			page: 'product'
+		}, {
+			title: "入库质量数据采集",
+			icon: icon_prefix + "renwu.png",
+			description: "入库质量数据采集",
+			useCount: 10000,
+			page: 'rkData'
+		}, {
+			title: "产出品质量数据采集",
+			icon: icon_prefix + "renwu.png",
+			description: "产出品质量数据采集",
+			useCount: 10000,
+			page: 'productData'
+		}, {
+			title: "质量数据采集",
+			icon: icon_prefix + "renwu.png",
+			description: "质量数据采集",
+			useCount: 10000,
+			page: 'dataCollect'
+		}
+	]
 }
 
 
@@ -119,49 +119,49 @@ export const us = {
  * other server 其他服务
  */
 export const os = {
-  data:[
-   //  {
-   //    title:"新闻中心",
-   //    icon:icon_prefix+"xinwen.png",
-   //    description:"新闻中心",
-   //    useCount:10000,
-	  // page:'helloWorld'
-   //  },{
-   //    title:"投票中心",
-   //    icon:icon_prefix+"toupiao.png",
-   //    description:"投票中心",
-   //    useCount:10000,
-	  // page:'helloWorld'
-   //  },{
-   //    title:"任务中心",
-   //    icon:icon_prefix+"renwu.png",
-   //    description:"任务中心",
-   //    useCount:10000,
-	  // page:'helloWorld'
-   //  },{
-   //    title:"文档中心",
-   //    icon:icon_prefix+"wendang.png",
-   //    description:"文档中心",
-   //    useCount:10000,
-	  // page:'helloWorld'
-   //  },{
-   //    title:"合同",
-   //    icon:icon_prefix+"hetong.png",
-   //    description:"合同",
-   //    useCount:10000,
-	  // page:'helloWorld'
-   //  },{
-   //    title:"会议",
-   //    icon:icon_prefix+"huiyi.png",
-   //    description:"会议",
-   //    useCount:10000,
-	  // page:'helloWorld'
-   //  },{
-   //    title:"客户关系",
-   //    icon:icon_prefix+"tongzhi.png",
-   //    description:"客户关系",
-   //    useCount:10000,
-	  // page:'helloWorld'
-   //  }
-  ]
+	data: [
+		//  {
+		//    title:"新闻中心",
+		//    icon:icon_prefix+"xinwen.png",
+		//    description:"新闻中心",
+		//    useCount:10000,
+		// page:'helloWorld'
+		//  },{
+		//    title:"投票中心",
+		//    icon:icon_prefix+"toupiao.png",
+		//    description:"投票中心",
+		//    useCount:10000,
+		// page:'helloWorld'
+		//  },{
+		//    title:"任务中心",
+		//    icon:icon_prefix+"renwu.png",
+		//    description:"任务中心",
+		//    useCount:10000,
+		// page:'helloWorld'
+		//  },{
+		//    title:"文档中心",
+		//    icon:icon_prefix+"wendang.png",
+		//    description:"文档中心",
+		//    useCount:10000,
+		// page:'helloWorld'
+		//  },{
+		//    title:"合同",
+		//    icon:icon_prefix+"hetong.png",
+		//    description:"合同",
+		//    useCount:10000,
+		// page:'helloWorld'
+		//  },{
+		//    title:"会议",
+		//    icon:icon_prefix+"huiyi.png",
+		//    description:"会议",
+		//    useCount:10000,
+		// page:'helloWorld'
+		//  },{
+		//    title:"客户关系",
+		//    icon:icon_prefix+"tongzhi.png",
+		//    description:"客户关系",
+		//    useCount:10000,
+		// page:'helloWorld'
+		//  }
+	]
 }
